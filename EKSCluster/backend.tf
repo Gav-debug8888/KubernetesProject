5c38363424d1db171310b3e218a88ef4ea7161a4
Gav-debug8888 <gavintsang1512@gmail.com>
2025-01-11 22:24:12 +0000

    Update backend.tf

--- EKSCluster/backend.tf
+++ EKSCluster/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "XX" # CHANGE WITH YOUR S3 BUCKET
+    bucket = "terraform-project88"
     key    = "K8inEKS/terraform.tfstate"
     region = "eu-west-2"
   }
